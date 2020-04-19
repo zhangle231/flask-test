e98dd8568d9933a45b089b22697e81d1306fa9d5
leo <zhangle231@163.com>
2020-04-19 23:03:29 +0800

    123

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/tmp3/flask-test/venv"
+set -gx VIRTUAL_ENV "/root/project/flask-test/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
