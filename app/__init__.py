557231e3655919dce4eb43d5bd91ceead3383736
root <zhangle231@163.com>
2020-04-27 17:32:59 +0800

    123

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,19 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__, instance_relative_config=True)
   
-  print(app.instance_path)
-  
   app.config.from_mapping(
       SECRET_KEY='dev',
       DATABASE=os.path.join(app.instance_path, 'my_db.sqlite'),
   )
   
+
+  if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
+
   # ensure the instance folder exists
   try:
     os.makedirs(app.instance_path)
@@ -26,7 +32,6 @@ def create_app(test_config=None):
 
   from . import db
   db.init_app(app)
-
 
   from . import auth
   app.register_blueprint(auth.bp)
---
+++ app/sqlalchemy/test.py
@@ -0,0 +1,40 @@
+import sqlalchemy
+
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///foo.db', echo=True)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  fullname = Column(String)
+  nickname = Column(String)
+  def __repr__(self):
+    return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+           self.name, self.fullname, self.nickname)
+
+Base.metadata.create_all(engine)
+
+ed_user = User(name='ed', fullname='Ed Jones', nickname='edsnickname')
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+session.add(ed_user)
+
+session.commit()
+
+our_user = session.query(User).filter_by(name='ed').first()
+
+print(our_user)
+
