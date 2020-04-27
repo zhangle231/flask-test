4e8367334d802d5471a0a0d11ca6b7706e2a4355
leo <zhangle231@163.com>
2020-04-27 22:00:38 +0800

    123

--- app/sqlalchemy/test.py
+++ app/sqlalchemy/test.py
@@ -8,6 +8,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy import Column, Integer, String
 
+from sqlalchemy import ForeignKey
+
+from sqlalchemy.orm import relationship
+
 Base = declarative_base()
 
 class User(Base):
@@ -19,6 +23,19 @@ class User(Base):
   def __repr__(self):
     return "<User(name='%s', fullname='%s', nickname='%s')>" % (
            self.name, self.fullname, self.nickname)
+
+class Address(Base):
+  __tablename__ = 'addresses'
+  id = Column(Integer, primary_key=True)
+  email_address = Column(String, nullable=False)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  user = relationship("User", back_populates="addresses")
+
+  def __repr__(self):
+    return "<Address(email_address='%s')>" % self.email_address
+
+
+User.addresses = relationship("Address", order_by=Address.id, back_populates="user")
 
 Base.metadata.create_all(engine)
 
@@ -38,3 +55,44 @@ our_user = session.query(User).filter_by(name='ed').first()
 
 print(our_user)
 
+our_user.nickname = 'eddie'
+
+print(session.dirty)
+
+session.commit()
+
+for instance in session.query(User).order_by(User.id):
+  print(instance.name,instance.fullname)
+
+
+jack = User(name='jack',fullname='Jack Bean',nickname='gjffdd')
+jack.addresses = [
+                Address(email_address='jack@google.com'),
+                Address(email_address='j25@yahoo.com')]
+
+print(jack.addresses)
+
+print(jack.addresses[1].user)
+
+#session.add(jack)
+#session.commit()
+
+jack = session.query(User).filter_by(name='jack').one()
+print(jack)
+print(jack.addresses)
+
+for u,a in session.query(User, Address).filter(User.id==Address.user_id)\
+                                       .filter(Address.email_address=='jack@google.com').all():
+  print(u)
+  print(a)
+
+print('--------------')
+
+jack = session.query(User).join(Address).filter(Address.email_address=='jack@google.com').all()
+print(jack)
+
+print('------------')
+
+query = session.query(User,Address).select_from(Address).join(User)
+for a in query:
+  print(a)
