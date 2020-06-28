65cbe8c19051f82869e350033d64d64f75220e93
zhangle231@163.com <zhangle231@163.com>
2020-06-28 22:15:51 +0800

    123

--- movie/scrapy/movie_scrapy/movie_scrapy/spiders/example.py
+++ movie/scrapy/movie_scrapy/movie_scrapy/spiders/example.py
@@ -7,10 +7,12 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ['http://www.dygangs.com/ys/']
 
     def parse(self, response):
-        yield { 
-                'text' : response.xpath('//table/tbody/tr[1]/td[1]/table/tbody')[0].get()
-        }
-
+        for base in response.xpath('//tr[1]/td[1]/table[1]/tbody/tr/td/a/img'):
+            yield { 
+                    'text' : base.xpath('./@alt').get(),
+                    'img' : base.xpath('./@src').get(),
+            }
+    
         #for quote in response.xpath('//tbody/tr/td/table/tbody'):
         #    yield {
         #            'text': quote.xpath("//td[2]/a/text()").getall(),
