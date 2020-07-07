8bbfa5e0c093c3d09f930219e7d645b1b77959e4
zhangle <zhangle231@163.com>
2020-07-07 09:34:35 +0000

    123

--- app/blog.py
+++ app/blog.py
@@ -12,10 +12,23 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
   db = get_db()
+
   posts = db.execute(
     'SELECT p.id, title, body, created, author_id, username' ' FROM post p JOIN user u ON p.author_id = u.id'
     ' ORDER BY created DESC' ).fetchall()
-  return render_template('blog/index.html', posts=posts)
+
+  menus = {'name':'1 test','is_leaf':0,
+           'children':[
+                    {'name':'1.1 test','is_leaf':1,'chlidren':None},
+                    {'name':'1.2 test','is_leaf':0,
+                     'children':[
+                            {'name':'1.2.1 test','is_leaf':1,'chlidren':None}
+                        ]}
+            ]
+          }
+
+  list_of_user = [{'username':'1','realname':'2','description':'desc'}]
+  return render_template('blog/index.html', posts=posts, menus=menus, list_of_user=list_of_user)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
