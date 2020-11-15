0f7c066d7a1cb560542bd9ab6c0530b3195ed662
zhangle <zhangle231@163.com>
2020-11-15 14:31:25 +0000

    monitor

--- monitor/src/app.py
+++ monitor/src/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/data.json')
+def data():
+    result = {}
+    result['result'] = 200
+    result['categories'] = ["衬衫","羊毛衫","裤子","高跟鞋","袜子"]
+    result['data'] = [15,20,36,50,18]
+    import time
+    time.sleep(1)
+    return result;
